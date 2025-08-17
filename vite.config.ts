130be4fcc607af1c966a4f77b3f3b18cba90db06
Nimmio <Benjamin@Wilhelmit.de>
2025-08-17 20:53:02 +0200

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/roadsurfer-coding-challenge/",
+  base: "/",
   test: {
     globals: true,
     environment: "jsdom",
